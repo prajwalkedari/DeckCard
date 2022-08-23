4358d95a470187f3a7b4d404498c43d16e5077ef
Prajwal Kedari <65669641+prajwalkedari@users.noreply.github.com>
2022-08-24 01:25:16 +0530

    Let Start

---
+++ source.js
@@ -0,0 +1,52 @@
+var uni_n=[
+	["U+1F0A1", "U+1F0B1", "U+1F0C1", "U+1F0D1 "], 
+	["U+1F0A2", "U+1F0B2", "U+1F0C2", "U+1F0D2 "], 
+	["U+1F0A3", "U+1F0B3", "U+1F0C3", "U+1F0D3 "], 
+	["U+1F0A4", "U+1F0B4", "U+1F0C4", "U+1F0D4 "], 
+	["U+1F0A5", "U+1F0B5", "U+1F0C5", "U+1F0D5 "], 
+	["U+1F0A6", "U+1F0B6", "U+1F0C6", "U+1F0D6 "], 
+	["U+1F0A7", "U+1F0B7", "U+1F0C7", "U+1F0D7 "], 
+	["U+1F0A8", "U+1F0B8", "U+1F0C8", "U+1F0D8 "], 
+	["U+1F0A9", "U+1F0B9", "U+1F0C9", "U+1F0D9 "], 
+	["U+1F0AA", "U+1F0BA", "U+1F0CA", "U+1F0DA "], 
+	["U+1F0AB", "U+1F0BB", "U+1F0CB", "U+1F0DB "], 
+	["U+1F0AC", "U+1F0BC", "U+1F0CC", "U+1F0DC "], 
+	["U+1F0AD", "U+1F0BD", "U+1F0CD", "U+1F0DD "], 
+	["U+1F0AE", "U+1F0BE", "U+1F0CE", "U+1F0DE "]
+];
+
+var uni_s =[
+	["🂡", "🂱", "🃁", "🃑 "], 
+	["🂢", "🂲", "🃂", "🃒 "], 
+	["🂣", "🂳", "🃃", "🃓 "], 
+	["🂤", "🂴", "🃄", "🃔 "], 
+	["🂥", "🂵", "🃅", "🃕 "], 
+	["🂦", "🂶", "🃆", "🃖 "], 
+	["🂧", "🂷", "🃇", "🃗 "], 
+	["🂨", "🂸", "🃈", "🃘 "], 
+	["🂩", "🂹", "🃉", "🃙 "], 
+	["🂪", "🂺", "🃊", "🃚 "], 
+	["🂫", "🂻", "🃋", "🃛 "], 
+	["🂬", "🂼", "🃌", "🃜 "], 
+	["🂭", "🂽", "🃍", "🃝 "], 
+	["🂮", "🂾", "🃎", "🃞 "]
+];
+
+var CardName =[
+	["Ace of Spades", "Ace of Hearts", "Ace of Diamonds", "Ace of Clubs "], 
+	["Two of Spades", "Two of Hearts", "Two of Diamonds", "Two of Clubs "], 
+	["Three of Spades", "Three of Hearts", "Three of Diamonds", "Three of Clubs "], 
+	["Four of Spades", "Four of Hearts", "Four of Diamonds", "Four of Clubs "], 
+	["Five of Spades", "Five of Hearts", "Five of Diamonds", "Five of Clubs "], 
+	["Six of Spades", "Six of Hearts", "Six of Diamonds", "Six of Clubs "], 
+	["Seven of Spades", "Seven of Hearts", "Seven of Diamonds", "Seven of Clubs "], 
+	["Eight of Spades", "Eight of Hearts", "Eight of Diamonds", "Eight of Clubs "], 
+	["Nine of Spades", "Nine of Hearts", "Nine of Diamonds", "Nine of Clubs "], 
+	["Ten of Spades", "Ten of Hearts", "Ten of Diamonds", "Ten of Clubs "], 
+	["Jack of Spades", "Jack of Hearts", "Jack of Diamonds", "Jack of Clubs "], 
+	["Knight of Spades", "Knight of Hearts", "Knight of Diamonds", "Knight of Clubs "], 
+	["Queen of Spades", "Queen of Hearts", "Queen of Diamonds", "Queen of Clubs "], 
+	["King of Spades", "King of Hearts", "King of Diamonds", "King of Clubs "]
+];
+
+var sim = {A:0,2:1,3:2,4:3,5:4,6:5,7:6,8:7,9:8,0:9,J:10,Q:11,K:13,H:1,S:0,D:2,C:3}
